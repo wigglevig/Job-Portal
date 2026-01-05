8ac60c591f34dbe73c4c1a544dfd9749b916922c
wigglevig <vignesh080903@gmail.com>
2026-01-06 02:08:42 +0530

    Job Search Performance Improvements

--- Backend/src/main/java/com/wiggle/springbootrest/repo/JobRepo.java
+++ Backend/src/main/java/com/wiggle/springbootrest/repo/JobRepo.java
@@ -1,13 +1,12 @@
 package com.wiggle.springbootrest.repo;
 
 
-import com.wiggle.springbootrest.model.JobPost;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import com.wiggle.springbootrest.model.JobPost;
 
 
 @Repository
